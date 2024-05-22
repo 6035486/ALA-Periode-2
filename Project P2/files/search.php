aada1a67bbacda9796e3683069c2aa25a7b7225a
joepk8 <joepjfklinkenberg@gmail.com>
2024-05-22 09:55:58 +0200

    search

--- Project P2/files/search.php
+++ Project P2/files/search.php
@@ -1,22 +1,54 @@
 <?php
 require_once('../helpers/helpers.php');
 if (isset($_POST["query"])) {
-    $results = search($db ,$_POST["query"]);
-    foreach ($results as $row) {
-        echo "<p>".$row["SerieTitel"]."<br>".$row["IMDBLink"]. "</p><br><br><br>";
-    }
-}?>
+    $results = search($db, $_POST["query"]);
+}
+$imageSrc = "../images/"
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../css/style.css">
 </head>
-<body>
-    <form method="post">
-        <input type="text" name="query" id="">
-        <input type="submit" value="submit">
-    </form>
+<body class="search">
+    <header>
+        <form method="post">
+            <input type="text" name="query" id="">
+            <input type="submit" value="submit">
+        </form>
+    </header>
+    <main>
+        <?php if (isset($results)) {
+             foreach($results as $result) { 
+                if ($result["Actief"] == "1") {
+                    ?> <div>
+                        <img src="<?php switch (strlen($result["SerieID"])) {
+                            case 1:
+                                echo $imageSrc . "0000" . $result["SerieID"] . ".jpg";
+                                break;
+                            
+                            case 2:
+                                echo $imageSrc . "000" . $result["SerieID"] . ".jpg";
+                                break;
+                            
+                            case 3:
+                                echo $imageSrc . "00" . $result["SerieID"] . ".jpg";
+                                break;
+                            case 4:
+                                echo $imageSrc . "0" . $result["SerieID"] . ".jpg";
+                                break;
+                            
+                            default:
+                                # code...
+                                break;
+                        } ?>" alt="" srcset="">
+                    </div> <?php
+                } else {
+                }
+              }}?>
+    </main>
 </body>
 </html>
--- Project P2/helpers/helpers.php
+++ Project P2/helpers/helpers.php
@@ -1,61 +1,53 @@
-<?php 
-require_once('../connect/connect.php');
+<?php
+require_once ('../connect/connect.php');
 
-function getActiveSeries($db){
-    $sql = "SELECT * FROM serie WHERE Actief = 1"; 
+function getActiveSeries($db)
+{
+    $sql = "SELECT * FROM serie WHERE Actief = 1";
     $result = $db->query($sql);
     return $result->fetchAll(PDO::FETCH_ASSOC);
 }
 
-function getNonActiveSeries($db){
+function getNonActiveSeries($db)
+{
     $sql = "SELECT * FROM serie WHERE Actief = 0";
     $result = $db->query($sql);
     return $result->fetchAll(PDO::FETCH_ASSOC);
 }
-function resetPassword($db){
-$testpassword = "Password";
-$password = password_hash($testpassword, PASSWORD_DEFAULT);
-$sql = 
-    "UPDATE klant
+function resetPassword($db)
+{
+    $testpassword = "Password";
+    $password = password_hash($testpassword, PASSWORD_DEFAULT);
+    $sql =
+        "UPDATE klant
     SET password = ?
     WHERE 1=1";
-$stm = $db->prepare($sql);
-$stm->execute([$password]);
-$stm->fetch(PDO::FETCH_ASSOC);}
+    $stm = $db->prepare($sql);
+    $stm->execute([$password]);
+    $stm->fetch(PDO::FETCH_ASSOC);
+}
 
-function checkPassword($db, $email, $password){
+function checkPassword($db, $email, $password)
+{
     $sql = "SELECT * FROM klant WHERE email = ?";
     $stm = $db->prepare($sql);
     $stm->execute([$email]);
     $selectedUser = $stm->fetch(PDO::FETCH_ASSOC);
     if (password_verify($password, $selectedUser["password"])) {
         return $selectedUser['KlantNr'];
-    }
-    else {
+    } else {
         return false;
     }
 }
 
-function search($db, $search){
-    $query = $db->prepare("SELECT * FROM serie WHERE SerieTitel = :search AND Actief = 1");
-    $query->execute(['search' => $search]);
+function search($db, $search)
+{
+    $searchLike = "%".$search."%";
+    $query = $db->prepare("SELECT * FROM serie WHERE SerieTitel LIKE :search LIMIT 10");
+    $query->execute(["search" => $searchLike]);
     $results = $query->fetchAll(PDO::FETCH_ASSOC);
 
-    if (empty($results)) {
-        $searchLike = "%". $search ."%";
-        $query = $db->prepare("SELECT * FROM serie WHERE SerieTitel LIKE :search AND Actief = 1");
-        $query->execute(["search" => $searchLike]);
-        $results = $query->fetchAll(PDO::FETCH_ASSOC);
-    }
-    if (empty($results)) {
-        $allResults = getActiveSeries($db);
-        $results = [];
-        foreach ($$allResults as $result) {
-            $distance = levenshtein($search, $result['SerieTitel']);
-            if($distance < 3) {
-                array_push($results, $result);
-            }
-        }
-    }
+    usort($results, fn($a, $b) => levenshtein($search, $a["SerieTitel"])<=> levenshtein($search, $b["SerieTitel"]));
+   
     return $results;
 }
